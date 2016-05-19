c3187640733346148c7338d6edc854314d0e9434
speedy32129 <ironspeeder@hotmail.com>
2016-05-19 10:36:58 -0400

    initial commit

---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :projects_show do
+  name 'Projects Show plugin'
+  author 'Jim McAleer'
+  description 'This plugin replaces the projects show page and displays a better subproject list.'
+  version '0.0.1'
+end
